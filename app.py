1fa02743201030c004751b3780f993ea3223d82b
Abhishek-Nyol <abhisheknyol704@gmail.com>
2026-02-16 16:19:02 +0530

    Initial commit - Web Application Created

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+import socket
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+
+    return jsonify({
+
+        "Project": "CI/CD Pipeline for Web Application",
+
+        "Status": "Running Successfully",
+
+        "Version": "1.0",
+
+        "Hostname": socket.gethostname(),
+
+        "Time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    })
+
+
+@app.route("/health")
+
+def health():
+
+    return "Application Healthy", 200
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=5000)
